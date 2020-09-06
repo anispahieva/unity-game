7c413d06c121891252f9d33214d985042a92449f
Ani <ani.spahieva25@gmail.com>
2020-09-06 23:31:18 +0200

    game over functionality added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public void EndGame() {
+        //logs game over in 
+        if(!gameHasEnded) {
+
+         gameHasEnded = true;
+         Debug.Log("Game Over");
+         Invoke("Restart", restartDelay);
+        }
+
+    }
+    void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
