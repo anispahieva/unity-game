674c16006185db89b7ac2f1a7ab2f304e5534df0
Ani <ani.spahieva25@gmail.com>
2020-09-06 19:08:51 +0200

    initialize project

---
+++ Karting/Assets/movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //add forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        } else if (Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        } 
+    }
+}
